aa569626ecad4c3c9556d1f5b376310bf778f9df
kienfullstack2004 <214240385+KienNguyenFullstack@users.noreply.github.com>
2025-06-01 08:47:35 +0700

    update

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require("dotenv").config();
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("bxtwpqoesklqlb2gim9u", "u4ko2ugjz4b3gzmg","y2bY8vqA9GsZNxNYqEFR", {
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER,process.env.DATABASE_PASSWORD,{
     host: process.env.DATABASE_HOST,
     dialect: 'mysql',
     logging: false
